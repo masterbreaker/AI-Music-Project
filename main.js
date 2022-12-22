c1e843e4f0555b197e2cceae2c0f47693f1ee929
Moradabad <75120768+masterbreaker@users.noreply.github.com>
2022-12-22 13:51:47 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 song1="";
 song2="";
 
+leftWristX=0
+leftWristY=0
+rightWristX=0
+rightWristY=0
+
 function preload(){
     song1=loadSound("music.mp3");
     song2=loadSound("music2.mp3");
@@ -12,9 +17,30 @@ function setup(){
     canvas.center();
     video=createCapture(VIDEO);
     video.hide();
+
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses);
 }
 
 function draw(){
     image(video,0,0,600,500);
 
+}
+
+function modelLoaded(){
+    console.log("posenet model is initialized")
+}
+
+function gotPoses(results){
+if(results.length>0)
+{
+    console.log(results);
+    leftWristX= results[0].pose.leftWrist.x;
+    leftWristY = results[0].pose.leftWrist.y;
+    console.log("lefwristX = " + leftWristX + "leftWristY = " + leftWristY );
+
+    rightWristX= results[0].pose.rightWrist.x;
+    rightWristY = results[0].pose.rightWrist.y;
+    console.log("rightwristX = " + rightWristX + "rightWristY = " + rightWristY );
+}
 }
