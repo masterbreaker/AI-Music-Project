bc6fcc779615d5d6cf4037f57c9034c8f5e832c1
Moradabad <75120768+masterbreaker@users.noreply.github.com>
2022-12-29 16:02:21 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -5,6 +5,10 @@ leftWristX=0
 leftWristY=0
 rightWristX=0
 rightWristY=0
+scoreleftwrist=0
+song1_status=""
+song2_status=""
+
 
 function preload(){
     song1=loadSound("music.mp3");
@@ -24,6 +28,19 @@ function setup(){
 
 function draw(){
     image(video,0,0,600,500);
+    fill("#ffb303")
+    stroke("#ffb303")
+    song1_status=song1.isPlaying();
+    song2_status=song2.isPlaying();
+    if(scoreleftwrist>0.2){
+        circle(leftWristX,leftWristY,20)
+        song2.stop();
+        if(song1_status==false){
+            song1.play();
+            document.getElementById("song_name").innerHTML="playing-harrypotter song"
+        }
+
+    }
 
 }
 
@@ -35,6 +52,7 @@ function gotPoses(results){
 if(results.length>0)
 {
     console.log(results);
+    scoreleftwrist=results[0].pose.keypoints[9].score;
     leftWristX= results[0].pose.leftWrist.x;
     leftWristY = results[0].pose.leftWrist.y;
     console.log("lefwristX = " + leftWristX + "leftWristY = " + leftWristY );
